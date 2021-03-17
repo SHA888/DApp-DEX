fdf9a1a755a8c8b6c2f11df826088e2f5bef07cc
SHA888 <kresnasucandra@gmail.com>
2021-03-18 05:52:33 +0800

    Uploaded from local host

---
+++ src/store/actions.js
@@ -0,0 +1,72 @@
+// WEB3
+export function web3Loaded(connection) {
+  return {
+    type: 'WEB3_LOADED',
+    connection
+  }
+}
+
+export function web3AccountLoaded(account) {
+  return {
+    type: 'WEB3_ACCOUNT_LOADED',
+    account
+  }
+}
+
+// TOKEN
+export function tokenLoaded(contract) {
+  return {
+    type: 'TOKEN_LOADED',
+    contract
+  }
+}
+
+// EXCHANGE
+export function exchangeLoaded(contract) {
+  return {
+    type: 'EXCHANGE_LOADED',
+    contract
+  }
+}
+
+export function cancelledOrdersLoaded(cancelledOrders) {
+  return {
+    type: 'CANCELLED_ORDERS_LOADED',
+    cancelledOrders
+  }
+}
+
+export function filledOrdersLoaded(filledOrders) {
+  return {
+    type: 'FILLED_ORDERS_LOADED',
+    filledOrders
+  }
+}
+
+export function allOrdersLoaded(allOrders) {
+  return {
+    type: 'ALL_ORDERS_LOADED',
+    allOrders
+  }
+}
+
+// Cancel Order
+export function orderCancelling() {
+  return {
+    type: 'ORDER_CANCELLING'
+  }
+}
+
+export function orderCancelled(order) {
+  return {
+    type: 'ORDER_CANCELLED',
+    order
+  }
+}
+
+// Fill Order
+export function orderFilling() {
+  return {
+    type: 'ORDER_FILLING'
+  }
+}
---
+++ src/store/interactions.js
@@ -0,0 +1,147 @@
+import Web3 from 'web3'
+import {
+  web3Loaded,
+  web3AccountLoaded,
+  tokenLoaded,
+  exchangeLoaded,
+  cancelledOrdersLoaded,
+  filledOrdersLoaded,
+  allOrdersLoaded,
+  orderCancelling,
+  orderCancelled,
+  orderFilling
+} from './actions'
+import Token from '../abis/Token.json'
+import Exchange from '../abis/Exchange.json'
+
+export const loadWeb3 = async (dispatch) => {
+  if(typeof window.ethereum!=='undefined'){
+    const web3 = new Web3(window.ethereum)
+    dispatch(web3Loaded(web3))
+    return web3
+  } else {
+    window.alert('Please install MetaMask')
+    window.location.assign("https://metamask.io/")
+  }
+}
+
+export const loadAccount = async (web3, dispatch) => {
+  const accounts = await web3.eth.getAccounts()
+  const account = await accounts[0]
+  if(typeof account !== 'undefined'){
+    dispatch(web3AccountLoaded(account))
+    return account
+  } else {
+    window.alert('Please login with MetaMask')
+    return null
+  }
+}
+
+export const loadToken = async (web3, networkId, dispatch) => {
+  try {
+    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
+    dispatch(tokenLoaded(token))
+    return token
+  } catch (error) {
+    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
+    return null
+  }
+}
+
+export const loadExchange = async (web3, networkId, dispatch) => {
+  try {
+    const exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address)
+    dispatch(exchangeLoaded(exchange))
+    return exchange
+  } catch (error) {
+    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
+    return null
+  }
+}
+
+export const loadAllOrders = async (exchange, dispatch) => {
+  // Fetch cancelled orders with the "Cancel" event stream
+  const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toBlock: 'latest' })
+  // Format cancelled orders
+  const cancelledOrders = cancelStream.map((event) => event.returnValues)
+  // Add cancelled orders to the redux store
+  dispatch(cancelledOrdersLoaded(cancelledOrders))
+
+  // Fetch filled orders with the "Trade" event stream
+  const tradeStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toBlock: 'latest' })
+  // Format filled orders
+  const filledOrders = tradeStream.map((event) => event.returnValues)
+  // Add cancelled orders to the redux store
+  dispatch(filledOrdersLoaded(filledOrders))
+
+  // Load order stream
+  const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0,  toBlock: 'latest' })
+  // Format order stream
+  const allOrders = orderStream.map((event) => event.returnValues)
+  // Add open orders to the redux store
+  dispatch(allOrdersLoaded(allOrders))
+}
+
+export const subscribeToEvents = async (exchange, dispatch) => {
+  exchange.events.Cancel({}, (error, event) => {
+    dispatch(orderCancelled(event.returnValues))
+  })
+}
+
+export const cancelOrder = (dispatch, exchange, order, account) => {
+  exchange.methods.cancelOrder(order.id).send({ from: account })
+  .on('transactionHash', (hash) => {
+     dispatch(orderCancelling())
+  })
+  .on('error', (error) => {
+    console.log(error)
+    window.alert('There was an error!')
+  })
+}
+
+export const fillOrder = (dispatch, exchange, order, account) => {
+  exchange.methods.fillOrder(order.id).send({ from: account })
+  .on('transactionHash', (hash) => {
+     dispatch(orderFilling())
+  })
+  .on('error', (error) => {
+    console.log(error)
+    window.alert('There was an error!')
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
