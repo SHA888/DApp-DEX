f84756537645a6382a1ab105de85037d8240c642
SHA888 <kresnasucandra@gmail.com>
2021-03-25 10:08:38 +0800

    Working on the Balance.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,6 +6,7 @@ import OrderBook from './OrderBook'
 import Trades from './Trades'
 import MyTransactions from './MyTransactions'
 import PriceChart from './PriceChart'
+import Balance from './Balance'
 
 class Content extends Component {
   componentWillMount() {
@@ -23,13 +24,7 @@ class Content extends Component {
       <div className="content">
         <div className="vertical-split">
           <div className="card bg-dark text-white">
-            <div className="card-header">
-              Card Title
-            </div>
-            <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="/#" className="card-link">Card link</a>
-            </div>
+            <Balance />
           </div>
           <div className="card bg-dark text-white">
             <div className="card-header">
