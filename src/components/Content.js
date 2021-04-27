e9bf412f1df26474f3e1f9bd160b71f828bf6f91
SHA888 <kresnasucandra@gmail.com>
2021-04-27 22:43:27 +0800

    Update Balance.js file

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { exchangeSelector } from "../store/selectors";
-import { loadAllOrders, subscribeToEvents } from "../store/interactions";
-import OrderBook from "./OrderBook";
-import Trades from "./Trades";
-import MyTransactions from "./MyTransactions";
-import PriceChart from "./PriceChart";
-import Balance from "./Balance";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { exchangeSelector } from '../store/selectors'
+import { loadAllOrders, subscribeToEvents } from '../store/interactions'
+import OrderBook from './OrderBook'
+import Trades from './Trades'
+import MyTransactions from './MyTransactions'
+import PriceChart from './PriceChart'
+import Balance from './Balance'
 
 class Content extends Component {
   componentWillMount() {
-    this.loadBlockchainData(this.props);
+    this.loadBlockchainData(this.props)
   }
 
   async loadBlockchainData(props) {
-    const { dispatch, exchange } = props;
-    await loadAllOrders(exchange, dispatch);
-    await subscribeToEvents(exchange, dispatch);
+    const { dispatch, exchange } = props
+    await loadAllOrders(exchange, dispatch)
+    await subscribeToEvents(exchange, dispatch)
   }
 
   render() {
@@ -25,15 +25,12 @@ class Content extends Component {
         <div className="vertical-split">
           <Balance />
           <div className="card bg-dark text-white">
-            <div className="card-header">Card Title</div>
+            <div className="card-header">
+              Card Title
+            </div>
             <div className="card-body">
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="/#" className="card-link">
-                Card link
-              </a>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
             </div>
           </div>
         </div>
@@ -44,14 +41,14 @@ class Content extends Component {
         </div>
         <Trades />
       </div>
-    );
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
-    exchange: exchangeSelector(state),
-  };
+    exchange: exchangeSelector(state)
+  }
 }
 
-export default connect(mapStateToProps)(Content);
+export default connect(mapStateToProps)(Content)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,66 +1,63 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
 function web3(state = {}, action) {
   switch (action.type) {
-    case "WEB3_LOADED":
-      return { ...state, connection: action.connection };
-    case "WEB3_ACCOUNT_LOADED":
-      return { ...state, account: action.account };
-    case "ETHER_BALANCE_LOADED":
-      return { ...state, balance: action.balance };
+    case 'WEB3_LOADED':
+      return { ...state,  connection: action.connection }
+    case 'WEB3_ACCOUNT_LOADED':
+      return { ...state, account: action.account }
+    case 'ETHER_BALANCE_LOADED':
+      return { ...state, balance: action.balance }
     default:
-      return state;
+      return state
   }
 }
 
 function token(state = {}, action) {
   switch (action.type) {
-    case "TOKEN_LOADED":
-      return { ...state, loaded: true, contract: action.contract };
-    case "TOKEN_BALANCE_LOADED":
-      return { ...state, balance: action.balance };
+    case 'TOKEN_LOADED':
+      return { ...state, loaded: true, contract: action.contract }
+    case 'TOKEN_BALANCE_LOADED':
+      return { ...state, balance: action.balance }
     default:
-      return state;
+      return state
   }
 }
 
 function exchange(state = {}, action) {
+  let index, data
+
   switch (action.type) {
-    case "EXCHANGE_LOADED":
-      return { ...state, loaded: true, contract: action.contract };
-    case "CANCELLED_ORDERS_LOADED":
-      return {
-        ...state,
-        cancelledOrders: { loaded: true, data: action.cancelledOrders },
-      };
-    case "FILLED_ORDERS_LOADED":
-      return {
-        ...state,
-        filledOrders: { loaded: true, data: action.filledOrders },
-      };
-    case "ALL_ORDERS_LOADED":
-      return { ...state, allOrders: { loaded: true, data: action.allOrders } };
-    case "ORDER_CANCELLING":
-      return { ...state, orderCancelling: true };
-    case "ORDER_CANCELLED":
+    case 'EXCHANGE_LOADED':
+      return { ...state, loaded: true, contract: action.contract }
+    case 'CANCELLED_ORDERS_LOADED':
+      return { ...state, cancelledOrders: { loaded: true, data: action.cancelledOrders } }
+    case 'FILLED_ORDERS_LOADED':
+      return { ...state, filledOrders: { loaded: true, data: action.filledOrders } }
+    case 'ALL_ORDERS_LOADED':
+      return { ...state, allOrders: { loaded: true, data: action.allOrders } }
+    case 'ORDER_CANCELLING':
+      return { ...state, orderCancelling: true }
+    case 'ORDER_CANCELLED':
       return {
         ...state,
         orderCancelling: false,
         cancelledOrders: {
           ...state.cancelledOrders,
-          data: [...state.cancelledOrders.data, action.order],
-        },
-      };
-    case "ORDER_FILLED":
+          data: [
+            ...state.cancelledOrders.data,
+            action.order
+          ]
+        }
+      }
+    case 'ORDER_FILLED':
       // Prevent duplicate orders
-      index = state.filledOrders.data.findIndex(
-        (order) => order.id === action.order.id
-      );
+      index = state.filledOrders.data.findIndex(order => order.id === action.order.id);
 
-      if (index === -1) {
-        data = [...state.filledOrders.data, action.order];
+      if(index === -1) {
+        data = [...state.filledOrders.data, action.order]
       } else {
-        data = state.filledOrders.data;
+        data = state.filledOrders.data
       }
 
       return {
@@ -68,34 +65,38 @@ function exchange(state = {}, action) {
         orderFilling: false,
         filledOrders: {
           ...state.filledOrders,
-          data,
-        },
-      };
+          data
+        }
+      }
 
-    case "ORDER_FILLING":
-      return { ...state, orderFilling: true };
+    case 'ORDER_FILLING':
+      return { ...state, orderFilling: true }
 
-    case "EXCHANGE_ETHER_BALANCE_LOADED":
-      return { ...state, etherBalance: action.balance };
-
-    case "EXCHANGE_TOKEN_BALANCE_LOADED":
-      return { ...state, tokenBalance: action.balance };
-
-    case "BALANCES_LOADING":
-      return { ...state, balancesLoading: true };
-
-    case "BALANCES_LOADED":
-      return { ...state, balancesLoading: false };
-
+    case 'EXCHANGE_ETHER_BALANCE_LOADED':
+      return { ...state, etherBalance: action.balance }
+    case 'EXCHANGE_TOKEN_BALANCE_LOADED':
+      return { ...state, tokenBalance: action.balance }
+    case 'BALANCES_LOADING':
+      return { ...state, balancesLoading: true }
+    case 'BALANCES_LOADED':
+      return { ...state, balancesLoading: false }
+    case 'ETHER_DEPOSIT_AMOUNT_CHANGED':
+      return { ...state, etherDepositAmount: action.amount }
+    case 'ETHER_WITHDRAW_AMOUNT_CHANGED':
+      return { ...state, etherWithdrawAmount: action.amount }
+    case 'TOKEN_DEPOSIT_AMOUNT_CHANGED':
+      return { ...state, tokenDepositAmount: action.amount }
+    case 'TOKEN_WITHDRAW_AMOUNT_CHANGED':
+      return { ...state, tokenWithdrawAmount: action.amount }
     default:
-      return state;
+      return state
   }
 }
 
 const rootReducer = combineReducers({
   web3,
   token,
-  exchange,
-});
+  exchange
+})
 
-export default rootReducer;
+export default rootReducer
